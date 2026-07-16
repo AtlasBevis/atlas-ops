d856fbd51ba2ab079e1957340bfbbbe51cd425f2
truongthanh.binh <truongthanh.binh@vietcredit.com.vn>
2026-07-16 18:13:35 +0700

    uupdate gitlab-runner

---
+++ gitlab-runner/ci/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+set +x
+
+IMAGE_REPO=$1
+IMAGE_TAG=$2
+
+if [[ -z "${IMAGE_REPO}" ]]; then
+  echo "IMAGE_REPO is required" >&2
+  exit 1
+fi
+
+if [[ -z "${IMAGE_TAG}" ]]; then
+  echo "IMAGE_TAG is required" >&2
+  exit 1
+fi
+
+echo "===== Build Image ${IMAGE_REPO}:${IMAGE_TAG} ====="
+
+buildctl-daemonless.sh build \
+--frontend dockerfile.v0 \
+--local context=. \
+--local dockerfile=. \
+--export-cache type=registry,ref=${IMAGE_REPO}:cache \
+--import-cache type=registry,ref=${IMAGE_REPO}:cache \
+--output type=image,\"name=${IMAGE_REPO}:${IMAGE_TAG},${IMAGE_REPO}:latest\",push=true
--- gitlab-runner/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [[ -n "${CI_PROJECT_DIR:-}" ]]; then
-  git config --global --add safe.directory "${CI_PROJECT_DIR}"
-fi
-
-exec "$@"
